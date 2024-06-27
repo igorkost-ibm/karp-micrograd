a835efe88fcf8176bb5d28981f504e04d205d1f6
Igor Kronos <igor.kronos@weather.com>
2024-06-27 13:46:33 +0300

    my

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,14 +1,16 @@
+import math
 
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self.label = label
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -50,6 +52,19 @@ class Value:
         out._backward = _backward
 
         return out
+
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        
+        def _backward():
+        self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+        
+        return out
+
 
     def backward(self):
 
